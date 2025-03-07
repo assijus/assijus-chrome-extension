c851eaeb307e8f35a737e83cb1c280c1c66c3449
Renato Crivano <renato.crivano@gmail.com>
2025-03-07 12:25:50 -0300

    manifest v3 - parcial

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,41 @@
+// Service worker lifecycle events
+self.addEventListener('install', (event) => {
+    console.log('Service worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activated');
+    event.waitUntil(clients.claim());
+});
+
 var redirectToLocalhost = function(request, sender, sendResponse) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                sendResponse({ success: true, data: JSON.parse(xhr.responseText) });
-            } else {
+    // Replace XMLHttpRequest with fetch API which is required in service workers
+    fetch(request.url, {
+            method: request.method,
+            headers: request.method === "POST" ? { "Content-Type": "application/json;charset=UTF-8" } : {},
+            body: request.method === "POST" ? JSON.stringify(request.data) : undefined
+        })
+        .then(response => {
+            return response.text().then(text => {
                 try {
-                    var resp = JSON.parse(xhr.responseText);
-                    sendResponse({ success: false, data: resp });
+                    const data = text ? JSON.parse(text) : {};
+                    if (response.ok) {
+                        sendResponse({ success: true, data: data });
+                    } else {
+                        sendResponse({ success: false, data: data });
+                    }
                 } catch (ex) {
-                    sendResponse({ success: false, statusText: xhr.statusText });
+                    sendResponse({ success: false, statusText: response.statusText });
                 }
-            }
-        }
-    };
-    xhr.open(request.method, request.url, true);
-    if (request.method == "POST") {
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.send(JSON.stringify(request.data));
-    } else {
-        xhr.send();
-    }
-    return true;
-}
+            });
+        })
+        .catch(error => {
+            sendResponse({ success: false, statusText: error.message });
+        });
+
+    return true; // Keep the message channel open
+};
 
 var senderCallback;
 var port = chrome.runtime.connectNative('br.jus.trf2.assijus');
@@ -35,7 +48,6 @@ port.onMessage.addListener(function(msg) {
 port.onDisconnect.addListener(function() {
     console.log("Disconnected");
 });
-
 
 var processMessage = function(request, sender, sendResponse) {
     if (request.url.substring(request.url.length - '/test-extension'.length) == '/test-extension') {
@@ -56,6 +68,6 @@ var processMessage = function(request, sender, sendResponse) {
         return false;
     }
     return true;
-}
+};
 
 chrome.runtime.onMessageExternal.addListener(processMessage);
